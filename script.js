d2570eb0432622bba77b6941e9d9aa3b119e6a06
Prasanjit Rout <routprasanjit933@gmail.com>
2024-01-05 22:18:59 +0530

    second commit in github

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
 let boxes = document.querySelectorAll(".box");
 let resetbtn = document.querySelector("#reset");
+
+let turnO = true; //player x, player O
+
+const winPatterns =[
+
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("box was clicked");
+        if (turnO){
+            box.innerText = "0";
+            turnO = false;
+
+        }
+        else{
+            box.innerText ="X";
+            turnO = true;
+        }
+        box.disabled ="true";
+        checkWinner();
+    });
+});
+
+checkWinner = () => {
+    for( let pattern of winPatterns) {
+        console.log(pattern[0], pattern[1], pattern[2]);
+    }
+}
